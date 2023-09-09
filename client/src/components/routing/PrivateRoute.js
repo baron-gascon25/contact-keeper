766485054a0fb47fe1b0b5cfadc94fa7a3b7e8d5
baron-gascon25 <baronalvarezgascon25@gmail.com>
2023-09-09 21:12:10 +0800

    Get contacts of user from database

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,12 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import AuthContext from "../../context/auth/authContext";
 
 const PrivateRoute = () => {
   const authContext = useContext(AuthContext);
-  const { isAuthenticated } = authContext;
+  const { isAuthenticated, loading, loadUser } = authContext;
 
-  return !isAuthenticated ? <Navigate to='/login' /> : <Outlet />;
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
+
+  return !isAuthenticated && !loading ? <Navigate to='/login' /> : <Outlet />;
 };
 
 export default PrivateRoute;
